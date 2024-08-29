6185834534b0a9ab660607dbce0a8c53e560bd89
JackFed <jack.fedurek@gmail.com>
2024-08-28 22:31:57 -0400

    Add more sprites for white pieces

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ function App() {
     <div className='game'>
       <Board />
       
+      <img src="assets/white/white-rook.png" alt="" />
       <img src="assets/black/black-pawn.png" alt="" />
       <img src="assets/black/black-bishop.png" alt="" />
       <img src="assets/black/black-knight.png" alt="" />
+      <img src="assets/black/black-rook.png" alt="" />
       <img src="assets/black/black-queen.png" alt="" />
+      <img src="assets/white/white-queen.png" alt="" />
       <img src="assets/white/white-bishop.png" alt="" />
     </div>
   )
