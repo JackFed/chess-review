acc8464ff493f40ffd6c3a6bd96f32efd25df6b0
JackFed <jack.fedurek@gmail.com>
2024-08-28 21:40:46 -0400

    Created assets for black pieces

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import Board from './Board'
+import Board from './Board';
+import "./styles/App.css";
 
 function App() {
 
   return (
-    <>
+    <div className='game'>
       <Board />
-    </>
+      
+      <img src="assets/black/black-pawn.png" alt="" />
+      <img src="assets/black/black-bishop.png" alt="" />
+      <img src="assets/black/black-knight.png" alt="" />
+      <img src="assets/black/black-queen.png" alt="" />
+      <img src="assets/white/white-bishop.png" alt="" />
+    </div>
   )
 }
 
