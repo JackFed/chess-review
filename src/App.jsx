f9acb57ef69c83a2659669cbe8d1aafa5f157e39
JackFed <jack.fedurek@gmail.com>
2024-08-28 23:55:31 -0400

    Add complete piece assets for black and white

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,18 @@ function App() {
     <div className='game'>
       <Board />
       
-      <img src="assets/white/white-rook.png" alt="" />
       <img src="assets/black/black-pawn.png" alt="" />
       <img src="assets/black/black-bishop.png" alt="" />
       <img src="assets/black/black-knight.png" alt="" />
       <img src="assets/black/black-rook.png" alt="" />
       <img src="assets/black/black-queen.png" alt="" />
+      <img src="assets/black/black-king.png" alt="" />
+      <img src="assets/white/white-pawn.png" alt="" />
+      <img src="assets/white/white-rook.png" alt="" />
       <img src="assets/white/white-queen.png" alt="" />
+      <img src="assets/white/white-knight.png" alt="" />
       <img src="assets/white/white-bishop.png" alt="" />
+      <img src="assets/white/white-king.png" alt="" />
     </div>
   )
 }
