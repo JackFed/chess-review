e20901ea65de7cddde62b4efc93588ab2efaad67
JackFed <jack.fedurek@gmail.com>
2024-08-29 22:51:01 -0400

    Add basic image on tiles

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,6 +1,6 @@
 import "./styles/Board.css";
 
-const Board = ({ playerColor = "White" }) => { 
+const Board = ({ playerColor = "Black" }) => { 
 
     const cols = ["a", "b", "c", "d", "e", "f", "g", "h"];
     const rows = ["8", "7", "6", "5", "4", "3", "2", "1"];
@@ -11,19 +11,24 @@ const Board = ({ playerColor = "White" }) => {
             let row = [];
             for (let j=0; j < cols.length; j++) {
                 let color = "White";
+                let img = "assets/black/black-pawn.png"
+                
                 if ((i+j) % 2 === 1) {
                     color = "Black";
+                    img = "assets/white/white-bishop.png"
                 }
-                
+
                 if (playerColor === "Black" ) {
                     row.push({
                         pos: cols[7-j].concat(rows[7-i]),
                         color: color,
+                        img: img,
                     });
                 } else {                          
                     row.push({
                         pos: cols[j].concat(rows[i]),
                         color: color,
+                        img: img,
                     });
                 }
             }
@@ -43,7 +48,8 @@ const Board = ({ playerColor = "White" }) => {
                 <div key={rowIndex} className="row">
                     {row.map((tile, colIndex) => (
                         <div key={colIndex} className={`square ${tile.color}`}>
-                            {tile.pos}
+                            
+                            <img className="piece" src={tile.img} alt="" />
                         </div>
                     ))}
                 </div>
