343c5ce0a889787fd396d0bf19e6dfdc7734e845
JackFed <jack.fedurek@gmail.com>
2024-09-06 17:00:38 -0400

    Add initial board setup for black start

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,6 @@ function App() {
   return (
     <div className='game'>
       <Board />
-      
-      <img src="assets/black/black-pawn.png" alt="" />
-      <img src="assets/black/black-knight.png" alt="" />
-      <img src="assets/black/black-bishop.png" alt="" />
-      <img src="assets/black/black-rook.png" alt="" />
-      <img src="assets/black/black-queen.png" alt="" />
-      <img src="assets/black/black-king.png" alt="" />
-      <img src="assets/white/white-pawn.png" alt="" />
-      <img src="assets/white/white-knight.png" alt="" />
-      <img src="assets/white/white-bishop.png" alt="" />
-      <img src="assets/white/white-rook.png" alt="" />
-      <img src="assets/white/white-queen.png" alt="" />
-      <img src="assets/white/white-king.png" alt="" />
     </div>
   )
 }
--- src/Board.jsx
+++ src/Board.jsx
@@ -1,6 +1,6 @@
 import "./styles/Board.css";
 
-const Board = ({ playerColor = "Black" }) => { 
+const Board = ({ playerColor = "black" }) => { 
 
     const cols = ["a", "b", "c", "d", "e", "f", "g", "h"];
     const rows = ["8", "7", "6", "5", "4", "3", "2", "1"];
@@ -10,21 +10,88 @@ const Board = ({ playerColor = "Black" }) => {
         for (let i=0; i < rows.length; i++) {
             let row = [];
             for (let j=0; j < cols.length; j++) {
-                let color = "White";
-                let img = "assets/black/black-pawn.png"
+                let color = "white";
+                let img = ""
+                let piece = "";
                 
                 if ((i+j) % 2 === 1) {
-                    color = "Black";
-                    img = "assets/white/white-bishop.png"
+                    color = "black";
                 }
 
-                if (playerColor === "Black" ) {
+                if (playerColor === "black" ) {
+                    if (rows[7-i] == 1) {
+                        switch (cols[7-j]) {
+                            case "a":
+                            case "h":
+                                img = "assets/white/white-rook.png";
+                                piece = "R";
+                                break;
+                            case "b":
+                            case "g":
+                                img = "assets/white/white-bishop.png";
+                                piece = "B";
+                                break;
+                            case "c":
+                            case "f":
+                                img = "assets/white/white-knight.png";
+                                piece = "N";
+                                break;
+                            case "d":
+                                img = "assets/white/white-queen.png";
+                                piece = "Q";
+                                break;
+                            case "e":
+                                img = "assets/white/white-king.png";
+                                piece = "K";
+                                break;
+
+                        }
+                    }
+                    if (rows[7-i] == 8) {
+                        switch (cols[7-j]) {
+                            case "a":
+                            case "h":
+                                img = "assets/black/black-rook.png";
+                                piece = "R";
+                                break;
+                            case "b":
+                            case "g":
+                                img = "assets/black/black-bishop.png";
+                                piece = "B";
+                                break;
+                            case "c":
+                            case "f":
+                                img = "assets/black/black-knight.png";
+                                piece = "N";
+                                break;
+                            case "d":
+                                img = "assets/black/black-queen.png";
+                                piece = "Q";
+                                break;
+                            case "e":
+                                img = "assets/black/black-king.png";
+                                piece = "K";
+                                break;
+
+                        }
+                    }
+                    if (i == 1) {
+                        img = "assets/white/white-pawn.png"
+                    }
+                    if (i == 6) {
+                        img = "assets/black/black-pawn.png"
+                    }
                     row.push({
                         pos: cols[7-j].concat(rows[7-i]),
                         color: color,
                         img: img,
+                        piece: piece,
                     });
-                } else {                          
+                } else {           
+                    if (rows[i] === 1) {
+                        console.log(`Rows: ${rows[7-i]}`)
+                        img = "assets/white/white-king.png"
+                    }               
                     row.push({
                         pos: cols[j].concat(rows[i]),
                         color: color,
@@ -48,7 +115,7 @@ const Board = ({ playerColor = "Black" }) => {
                 <div key={rowIndex} className="row">
                     {row.map((tile, colIndex) => (
                         <div key={colIndex} className={`square ${tile.color}`}>
-                            
+                            {/* {tile.pos} */}
                             <img className="piece" src={tile.img} alt="" />
                         </div>
                     ))}
